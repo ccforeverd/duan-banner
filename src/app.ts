918d6641a4a6c7e7b6d9340c61da0d0349ce9ebc
ccforeverd <zh1045456074@163.com>
2021-01-11 19:23:54 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,20 @@
+import 'reflect-metadata'
 import * as fs from 'fs'
 import * as path from 'path'
+import Koa from 'koa'
+import KoaRouter from 'koa-router'
 
 console.log(fs.readFileSync(path.join(__dirname, '../package.json'), 'utf-8'))
 
+const app = new Koa()
+
+const router = new KoaRouter()
+router.get('/', (ctx) => {
+  ctx.body = 'hello world'
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(8456, '0.0.0.0')
