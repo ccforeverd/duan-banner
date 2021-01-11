ee89f12120f664c768ce5ba35ae95c1538d36104
ccforeverd <zh1045456074@163.com>
2021-01-12 00:35:25 +0800

    feat: add pug

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,28 @@ import * as fs from 'fs'
 import * as path from 'path'
 import Koa from 'koa'
 import KoaRouter from 'koa-router'
+import KoaViews from 'koa-views'
 
 console.log(fs.readFileSync(path.join(__dirname, '../package.json'), 'utf-8'))
 
 const app = new Koa()
 
 const router = new KoaRouter()
-router.get('/', (ctx) => {
-  ctx.body = 'hello world'
+router.get('/', async (ctx) => {
+  // ctx.body = 'hello world'
+  await render().call(ctx, 'banner.pug', {
+    youAreUsingPug: true
+  })
 })
 
+const render = KoaViews(__dirname, {
+  extension: 'pug'
+})
+
+app.context.render = render()
+
 app
+  // .use(render)
   .use(router.routes())
   .use(router.allowedMethods())
 
